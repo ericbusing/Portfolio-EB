41857f1a4c007153519c7dd47d23d2774681bc92
eric <busing.eric@gmail.com>
2022-11-04 12:51:14 +0100

    changement contenu + dark/light mode

---
+++ js/darkMode.js
@@ -0,0 +1,13 @@
+const btnToggle = document.querySelector(".btn-toggle");
+
+btnToggle.addEventListener("click", () => {
+  const body = document.body;
+
+  if (body.classList.contains("dark")) {
+    body.classList.add("light");
+    body.classList.remove("dark");
+  } else if (body.classList.contains("light")) {
+    body.classList.add("dark");
+    body.classList.remove("light");
+  }
+});
