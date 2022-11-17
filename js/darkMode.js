32d2e73911d692f9e387cb9960f2904630990ada
eric <busing.eric@gmail.com>
2022-11-17 15:11:11 +0100

    changement nav desktop et mobil/tablet

--- js/darkMode.js
+++ js/darkMode.js
@@ -1,6 +1,19 @@
 const btnToggle = document.querySelector(".btn-toggle");
+const asideBtnToggle = document.querySelector(".aside-btn-toggle");
 
 btnToggle.addEventListener("click", () => {
+  const body = document.body;
+
+  if (body.classList.contains("dark")) {
+    body.classList.add("light");
+    body.classList.remove("dark");
+  } else if (body.classList.contains("light")) {
+    body.classList.add("dark");
+    body.classList.remove("light");
+  }
+});
+
+asideBtnToggle.addEventListener("click", () => {
   const body = document.body;
 
   if (body.classList.contains("dark")) {
