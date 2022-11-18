9b78536bd0c43243129f9f5008547f5062e35475
eric <busing.eric@gmail.com>
2022-11-18 12:19:13 +0100

    email.js OK

---
+++ js/contact.js
@@ -0,0 +1,26 @@
+require("dotenv").config({ path: "../.env" });
+require("../");
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "MY_SERVICE_ID";
+  const templateID = "MY_TEMPLETE_ID";
+
+  emailjs
+    .send(serviceID, templateID, params)
+    .then((res) => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
+      console.log(res);
+      alert("Your message sent successfully!!");
+    })
+    .catch((err) => {
+      console.log(err);
+      alert("There is an error!!");
+    });
+}
